d8ea2cdec5a01b2d5e133f663ac7c1610e3b1231
gengjian1203 <gengjian1203@foxmail.com>
2022-03-27 00:14:02 +0800

    feat:(PageCss_DetailCssLoading)完善加载样式

--- src/config/code/PageCss_DetailCssLoading.js
+++ src/config/code/PageCss_DetailCssLoading.js
@@ -1,17 +1,18 @@
 export const content = [
   {
-    title: "loading布局",
+    title: "loading1布局",
     lang: "text/html",
     code: `
-<div class="iconLoading" />
+<div class="iconLoading1">
+</div>
     `,
   },
   {
-    title: "loading样式",
+    title: "loading1样式",
     lang: "text/css",
     code: `
 <style>
-  .iconLoading {
+  .iconLoading1 {
     width: 100px;
     height: 100px;
     border: 6px solid #eee;
@@ -27,6 +28,164 @@ export const content = [
     to {
       transform: rotate(360deg);
     }
+  }
+</style>
+    `,
+  },
+  {
+    title: "loading2布局",
+    lang: "text/html",
+    code: `
+<div class="loadEffect2">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+    `,
+  },
+  {
+    title: "loading2样式",
+    lang: "text/css",
+    code: `
+<style>
+  .loadEffect2 {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    margin: 0 auto;
+    margin-top:100px;
+  }
+  .loadEffect2 span {
+    display: inline-block;
+    width: 30px;
+    height: 10px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    background: #000;
+    position: absolute;
+    -webkit-animation: load2 1.04s ease infinite;
+  }
+  @-webkit-keyframes load2 {
+    0%{
+        opacity: 1;
+    }
+    100%{
+        opacity: 0.2;
+    }
+  }
+  .loadEffect2 span:nth-child(1) {
+    left: 0;
+    top: 50%;
+    margin-top:-5px;
+    -webkit-animation-delay:0.13s;
+  }
+  .loadEffect2 span:nth-child(2) {
+    left: 10px;
+    top: 20px;
+    -webkit-transform: rotate(45deg);
+    -webkit-animation-delay:0.26s;
+  }
+  .loadEffect2 span:nth-child(3) {
+    left: 50%;
+    top: 10px;
+    margin-left: -15px;
+    -webkit-transform: rotate(90deg);
+    -webkit-animation-delay:0.39s;
+  }
+  .loadEffect2 span:nth-child(4) {
+    top: 20px;
+    right:10px;
+    -webkit-transform: rotate(135deg);
+    -webkit-animation-delay:0.52s;
+  }
+  .loadEffect2 span:nth-child(5) {
+    right: 0;
+    top: 50%;
+    margin-top:-5px;
+    -webkit-transform: rotate(180deg);
+    -webkit-animation-delay:0.65s;
+  }
+  .loadEffect2 span:nth-child(6) {
+    right: 10px;
+    bottom:20px;
+    -webkit-transform: rotate(225deg);
+    -webkit-animation-delay:0.78s;
+  }
+  .loadEffect2 span:nth-child(7) {
+    bottom: 10px;
+    left: 50%;
+    margin-left: -15px;
+    -webkit-transform: rotate(270deg);
+    -webkit-animation-delay:0.91s;
+  }
+  .loadEffect2 span:nth-child(8) {
+    bottom: 20px;
+    left: 10px;
+    -webkit-transform: rotate(315deg);
+    -webkit-animation-delay:1.04s;
+  }
+</style>
+    `,
+  },
+  {
+    title: "loading3布局",
+    lang: "text/html",
+    code: `
+<div class="loading3">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+    `,
+  },
+  {
+    title: "loading3样式",
+    lang: "text/css",
+    code: `
+<style>
+  .loading3 {
+    width: 80px;
+    height: 40px;
+    margin: 0 auto;
+    margin-top:100px;
+  }
+  .loading3 span {
+    display: inline-block;
+    width: 8px;
+    height: 100%;
+    border-radius: 4px;
+    background: lightgreen;
+    -webkit-animation: load3 1.04s ease infinite;
+  }
+  @-webkit-keyframes load3 {
+    0%,100%{
+        height: 40px;
+        background: lightgreen;
+    }
+    50%{
+        height: 60px;
+        margin-top: -20px;
+        background: lightblue;
+    }
+  }
+  .loading3 span:nth-child(2) {
+    -webkit-animation-delay:0.13s;
+  }
+  .loading3 span:nth-child(3) {
+    -webkit-animation-delay:0.26s;
+  }
+  .loading3 span:nth-child(4) {
+    -webkit-animation-delay:0.39s;
+  }
+  .loading3 span:nth-child(5) {
+    -webkit-animation-delay:0.52s;
   }
 </style>
     `,
